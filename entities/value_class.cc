30b8f958d222a0265571b0629c454fe0d61c34a4
Ivan Subotic <400790+subotic@users.noreply.github.com>
2020-11-27 17:27:26 +0100

    reformat

--- entities/value_class.cc
+++ entities/value_class.cc
@@ -8,10 +8,14 @@
 #include "value_class.h"
 
 namespace dsp {
-    ValueClass::ValueClass(const std::string class_label,
-                           const std::string class_description) :
-                           class_label_(class_label),
-                           class_description_(class_description) {
-        id_ = uuid::generate_uuid_v4();
-    }
+
+ValueClass::ValueClass(
+    const std::string class_label,
+    const std::string class_description
+) :
+    class_label_(class_label),
+    class_description_(class_description) {
+  id_ = uuid::generate_uuid_v4();
+}
+
 }
