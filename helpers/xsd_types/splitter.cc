b3f796da68e59a6bcbc220d2604c39c0f60b44bd
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2020-12-18 19:45:43 +0100

    ...

--- helpers/xsd_types/splitter.cc
+++ helpers/xsd_types/splitter.cc
@@ -2,4 +2,67 @@
 // Created by Lukas Rosenthaler on 18.12.20.
 //
 
+#include <string>
+#include <vector>
 #include "splitter.h"
+
+namespace xsd {
+
+/*!
+ * Split a string into a vector of parts at the locations where the split string fits
+ * @param strval String to be splitted
+ * @param splitter The token that is used to split
+ * @return Vector of strings
+ */
+std::vector<std::string> split(const std::string &strval, const std::string &splitter) {
+  size_t old_pos = 0;
+  size_t pos;
+  int len = splitter.length();
+  std::vector<std::string> result;
+  while ((pos = strval.find(splitter, old_pos)) != std::string::npos) {
+    result.push_back(strval.substr(old_pos, pos - old_pos));
+    old_pos = pos + len;
+  }
+  result.push_back(strval.substr(old_pos, std::string::npos));
+  return result;
+}
+
+/*!
+ * Split a string at a given position
+ * @param strval  String to be splitted
+ * @param pos Position where to split
+ * @param len Number of characters to remove at the split position
+ * @return Split result struct
+ */
+SplitResult split_at(const std::string &strval, size_t pos, int len) {
+  std::string s1 = strval.substr(0, pos);
+  std::string s2 = (pos == std::string::npos) ? "" : strval.substr(pos + len, std::string::npos);
+  SplitResult result = {(pos != std::string::npos), s1, s2};
+  return result;
+}
+
+/*!
+ * Split a string at the first occurence of the split character
+ * @param strval String to be splitted
+ * @param splitter Split character
+ * @return Split result struct
+ */
+SplitResult split_first(const std::string &strval, char splitter) {
+  size_t pos = strval.find(splitter);
+  return split_at(strval, pos);
+}
+
+/*!
+ * Split a string at the split token
+ * @param strval String to be splitted
+ * @param splitter Split token
+ * @return Split result struct
+ */
+SplitResult split_first(const std::string &strval, const std::string &splitter) {
+  size_t pos = strval.find(splitter);
+  return split_at(strval, pos, splitter.length());
+}
+
+
+
+}
--- helpers/xsd_types/xsd_q_name.cc
+++ helpers/xsd_types/xsd_q_name.cc
@@ -2,4 +2,63 @@
 // Created by Lukas Rosenthaler on 18.12.20.
 //
 
-#include "q_name.h"
+#include <regex>
+
+#include "xsd_q_name.h"
+#include "xsd_error.h"
+#include "splitter.h"
+
+static const char file_[] = __FILE__;
+
+namespace xsd {
+
+QName::QName(const std::string &strval) : QName() {
+  parse(strval);
+  enforce_restrictions();
+}
+
+QName::QName(const std::string &strval, const std::shared_ptr<Restriction> &restriction) : QName(strval) {
+  restrictions_.push_back(restriction);
+  enforce_restrictions();
+}
+
+QName::QName(const std::string &strval, const std::vector<std::shared_ptr<Restriction>> &restrictions) : QName(strval) {
+  restrictions_ = restrictions;
+  enforce_restrictions();
+}
+
+QName &QName::operator=(const std::string &strval) {
+  set(strval);
+  enforce_restrictions();
+  return *this;
+}
+
+void QName::set(const std::string &strval) {
+  parse(strval);
+  enforce_restrictions();
+}
+
+std::ostream &QName::print_to_stream(std::ostream &out_stream) const {
+  if (!prefix_.empty()) out_stream << prefix_ << ":";
+  out_stream << name_;
+  return out_stream;
+}
+
+void QName::parse(const std::string &strval) {
+  SplitResult split_result = split_first(strval, ':');
+  if (split_result.success) {
+    prefix_ = split_result.first;
+    name_ = split_result.second;
+  } else {
+    name_ = strval;
+  }
+  if (!prefix_.empty() && !std::regex_match(prefix_, std::regex(R"(([a-zA-Z_\xC0-\xD6\xD8-\xF6])([a-zA-Z0-9_·\-\xC0-\xD6\xD8-\xF6])+)"))) {
+    throw Error(file_, __LINE__, "Prefix contains non-valid characters!");
+  }
+  if (!std::regex_match(name_, std::regex(R"(([a-zA-Z_\xC0-\xD6\xD8-\xF6])([a-zA-Z0-9_·\-\xC0-\xD6\xD8-\xF6])+)"))) {
+    throw Error(file_, __LINE__, "Name contains non-valid characters!");
+  }
+}
+
+
+}
