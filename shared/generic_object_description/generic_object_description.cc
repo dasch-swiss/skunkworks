24b4fa6766b0074e7ffa2ebe44077dc52f387fb7
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2021-01-05 15:01:22 +0100

    Cherry pick from moving

--- shared/generic_object_description/generic_object_description.cc
+++ shared/generic_object_description/generic_object_description.cc
@@ -8,20 +8,6 @@ const char file_[] = __FILE__;
 
 namespace dsp {
 
-template<typename T>
-void GenericObjectDescription::member(const std::string& name, const T& data) {
-  data_[name] = std::make_shared<T>(data);
-}
-
-template<typename T>
-T GenericObjectDescription::member(const std::string& name) const {
-  try {
-    T* ptr = dynamic_cast<T*>(data_.at(name).get());
-    return *ptr;
-  } catch (const std::out_of_range &err) {
-    throw Error(file_, __LINE__, "GenericObjectDescription has no member \"" + name + "\".");
-  }
-}
 
 bool GenericObjectDescription::has_member(const std::string& name) const {
   try {
