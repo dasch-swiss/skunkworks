5695e38d0daff149b204c7cb0d197f428e9e9532
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2021-01-05 16:33:09 +0100

    Deleted unnecessary files

--- data_model/in_mem_kv_store.h
+++ data_model/in_mem_kv_store.h
@@ -10,20 +10,13 @@
 #include <unordered_map>
 #include <vector>
 
-#include "external/nlohmann/json.hpp"
 
-#include "shared/xsd_types/xsd.h"
-#include "shared/dsp_types/id.h"
+#include "store.h"
 
-#include "project.h"
-
-#include "adapter.h"
-
-#include "key_value_store.h"
 
 namespace dsp {
 
-class InMemStore : public KeyValueStore {
+class InMemStore {
   /*!
    * This class implements an in-memory key-value store. It is a singleton with a static method "getInstance" which
    * is used to get access to the stire
@@ -36,27 +29,27 @@ class InMemStore : public KeyValueStore {
    * @param id dsp identifier
    * @param data
    */
-  void create(const dsp::Identifier &id, const std::vector<uint8_t> data) override;
+  void create(const dsp::Identifier &id, const std::vector<uint8_t> data) ;
 
   /*!
    * Read the data for a given key
    * @param id
    * @return
    */
-  std::vector<uint8_t> read(const dsp::Identifier &id) override;
+  std::vector<uint8_t> read(const dsp::Identifier &id) ;
 
   /*!
    * Update the data of a given key
    * @param id
    * @param data
    */
-  void update(const dsp::Identifier &id, const std::vector<uint8_t> data) override;
+  void update(const dsp::Identifier &id, const std::vector<uint8_t> data) ;
 
   /*!
    * Remove a given key and it's data from the key value store
    * @param id
    */
-  void remove(const dsp::Identifier &id) override;
+  void remove(const dsp::Identifier &id) ;
 
   /*!
    * No copy constructor
@@ -68,7 +61,7 @@ class InMemStore : public KeyValueStore {
    */
   void operator=(InMemStore const&) = delete;
  private:
-  std::unordered_map<dsp::Identifier, std::vector<uint8_t>> kvstore;
+  std::unordered_map<dsp::Identifier, std::vector<uint8_t>> kvstore_;
 };
 
 }
--- data_model/key_value_store.cc
+++
@@ -1,5 +0,0 @@
-//
-// Created by Lukas Rosenthaler on 02.01.21.
-//
-
-#include "key_value_store.h"
